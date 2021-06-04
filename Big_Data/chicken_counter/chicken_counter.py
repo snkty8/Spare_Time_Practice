c2dbd7920d24b3bc6946d5dee56d0c2a66c5d431
snkty8 <78936833+snkty8@users.noreply.github.com>
2021-06-04 15:16:38 -0400

    word counter py

---
+++ Big_Data/chicken_counter/chicken_counter.py
@@ -0,0 +1,11 @@
+from mrjob.job import MRJob
+class Chicken_count(MRJob):
+   def mapper(self, _, line):
+       for word in line.split():
+           if word.lower() == "chicken":
+               yield "chicken", 1
+
+   def reducer(self, key, values):
+       yield key, sum(values)
+if __name__ == "__main__":
+   Chicken_count.run()
